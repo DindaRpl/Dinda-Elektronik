88827df4546db4395bb4f6de00a16a8a13a919ad
DindaRpl <zahraardian206@gmail.com>
2026-03-12 15:35:25 +0700

    update codingan bagian App

--- src/web-jadi/src/App.jsx
+++ src/web-jadi/src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard"; 
-// 1. IMPORT file yang baru kita buat
 import ManageProducts from "./pages/ManageProducts"; 
-import ManageUsers from "./pages/ManageUsers"; // Pastikan path filenya benar ya!
+import ManageUsers from "./pages/ManageUsers"; 
+// 1. IMPORT file ManageCategories yang baru saja kamu buat
+import ManageCategories from "./pages/ManageCategories"; 
 
 function App () {
     return(
@@ -12,13 +13,16 @@ function App () {
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
             
-            {/* 2. TAMBAHKAN rute untuk Kelola Produk */}
+            {/* Rute untuk Kelola Produk */}
             <Route path="/kelola-produk" element={<ManageProducts />} />
 
-            {/* 3. TAMBAHKAN rute untuk Kelola User biar nggak mental ke login */}
+            {/* Rute untuk Kelola User */}
             <Route path="/kelola-user" element={<ManageUsers />} />
             
-            {/* Rute default: Kalau alamat ngawur, lempar ke login */}
+            {/* 2. TAMBAHKAN rute untuk Kelola Kategori */}
+            <Route path="/kelola-kategori" element={<ManageCategories />} />
+            
+            {/* Rute default: Jika path tidak ditemukan, lempar ke login */}
             <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
     );
